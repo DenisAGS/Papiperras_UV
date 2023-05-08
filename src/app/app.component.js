f3751782d625bab71b2d03b01809fb7e3f5a982d
DAGS <71294134+DenisAGS@users.noreply.github.com>
2023-05-08 09:41:16 -0600

    Update app.component.js

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,6 +1,1 @@
-const navbarIcon = document.querySelector('.navbar-icon');
-const navbarList = document.querySelector('.navbar ul');
 
-navbarIcon.addEventListener('click', function() {
-  navbarList.classList.toggle('show');
-});
