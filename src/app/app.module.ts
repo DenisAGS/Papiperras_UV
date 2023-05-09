27139ce050b7f7c5004d3b4b5c395e9eda45dd04
DAGS <71294134+DenisAGS@users.noreply.github.com>
2023-05-08 22:27:01 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+/*import { MatIconModule} from '@angular/material/icon';*/
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing-module';
@@ -17,7 +19,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
