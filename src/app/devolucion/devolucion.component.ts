ad3812a7fcfe563119a762009378722b1636f188
DAGS <71294134+DenisAGS@users.noreply.github.com>
2023-05-12 08:36:50 -0600

    Add files via upload

---
+++ src/app/devolucion/devolucion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-devolucion',
+  templateUrl: './devolucion.component.html',
+  styleUrls: ['./devolucion.component.css']
+})
+export class DevolucionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
