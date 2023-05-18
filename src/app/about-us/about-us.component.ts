02165eb087071dd061d9494c1c7bc9b499c920be
DenisAGS <denissearelyg@gmail.com>
2023-05-18 08:08:07 -0600

    update page about us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
-
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
