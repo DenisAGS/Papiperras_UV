ca77e42be2084c695e8a99184011b9d2f356e836
sergiojmnz <61261358+sergiojmnz@users.noreply.github.com>
2023-05-20 19:45:07 -0600

    se me cayo el pipi :(

---
+++ src/app/publicidad/publicidad1/publicidad1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publicidad1',
+  templateUrl: './publicidad1.component.html',
+  styleUrls: ['./publicidad1.component.css']
+})
+export class Publicidad1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
