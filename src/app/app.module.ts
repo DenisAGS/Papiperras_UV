d9f7d7aaef9498ad32aa3481d81592c21faa07d5
DAGS <71294134+DenisAGS@users.noreply.github.com>
2023-05-23 16:00:51 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,32 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrincipalComponent } from './principal/principal.component';
-import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DevolucionComponent } from './devolucion/devolucion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { HeaderPageComponent } from './header-page/header-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Publicidad2Component } from './publicidad/publicidad2/publicidad2.component';
+import { PubOfertasComponent } from './ofertas/pub-ofertas/pub-ofertas.component';
+import { PublicarPComponent } from './publicar-productos/publicar-p/publicar-p.component' ;
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    LoginComponent
+    AboutUsComponent,
+    DevolucionComponent,
+    InventarioComponent,
+    PreguntasFrecuentesComponent,
+    TutorialComponent,
+    HeaderPageComponent,
+    SidebarComponent,
+    Publicidad2Component,
+    PubOfertasComponent,
+    PublicarPComponent
   ],
   imports: [
     BrowserModule,
