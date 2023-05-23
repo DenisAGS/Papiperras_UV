fa79d678d6e1f687194bdb57633355dd9f606eea
DAGS <71294134+DenisAGS@users.noreply.github.com>
2023-05-23 12:22:30 -0600

    Update app-routing-module.ts

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -14,6 +14,7 @@ import { TutorialComponent } from './tutorial/tutorial.component';
 import { Publicidad2Component } from './publicidad/publicidad2/publicidad2.component';
 import { PubOfertasComponent } from './ofertas/pub-ofertas/pub-ofertas.component';
 import { PublicarPComponent } from './publicar-productos/publicar-p/publicar-p.component';
+import { ReclamosFormularioComponent } from './reclamos-formulario/reclamos-formulario.component';
 
 const routes: Routes = [  
   {
@@ -55,6 +56,10 @@ const routes: Routes = [
   {
     path: 'publicar-productos',
     component: PublicarPComponent
+  },
+  {
+    path:'reclamos-formulario',
+    component: ReclamosFormularioComponent
   }
 ];
 
