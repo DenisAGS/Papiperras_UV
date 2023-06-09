3ca98f7ea2eb2ae81b2c4c10b0748b777094b3e6
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2023-06-09 02:13:48 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination'; //npm install ngx-pagination --save
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 /*import { MatIconModule} from '@angular/material/icon';*/
 
 
@@ -41,7 +44,9 @@ import { ProblemasProductosComponent } from './problemas-productos/problemas-pro
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
