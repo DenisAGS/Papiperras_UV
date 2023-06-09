ec3b00c0be24ec88bb15696ba692a84935e79935
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2023-06-09 02:12:36 -0600

    Update problemas-productos.component.ts

--- src/app/problemas-productos/problemas-productos.component.ts
+++ src/app/problemas-productos/problemas-productos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ProblemasServices } from '../services/problemas.service';
+import { ProblemasServices } from '../services/problemas.services';
 
 @Component({
   selector: 'app-problemas-productos',
@@ -9,23 +9,91 @@ import { ProblemasServices } from '../services/problemas.service';
 })
 export class ProblemasProductosComponent implements OnInit {
 
-  constructor(private router: Router, private problemasServicio: ProblemasServices  ) { }
+  constructor(private router: Router, private problemasService: ProblemasServices  ) {}
   
   redirectToReclamos(){
     this.router.navigate(['/reclamos-formulario']);
   }
 
   data : any[] = [];
+  elementosPorPagina = 5;
+  paginaActual = 1;
 
-  obtenerDatosJson(){
-    this.problemasServicio.getJsonData().subscribe(data => {
-      this.data = data;
+  searchText : any;
+
+  dataPagina : any[] = [];//datos para la paginacion
+  dataCompleta : any[] = [];//datos para la paginacion
+  
+  onPageChange(event: any) {
+    this.paginaActual = event; // Actualizar la página actual con el valor proporcionado por el evento
+    this.actualizarDatosTabla(); // Método para actualizar los datos de la tabla según la página actual
+  }
+
+  actualizarDatosTabla() {//actualizar cada vez que se haga cambio de pagina en la tabla
+    const indiceInicial = (this.paginaActual - 1) * this.elementosPorPagina;
+    const indiceFinal = indiceInicial + this.elementosPorPagina;
+    this.dataPagina = this.dataCompleta.slice(indiceInicial, indiceFinal);
+  }
+
+  /* **********************************FILTRO************************************************************/
+  
+ 
+  datosFiltrados : any[] = [];
+  
+  filtrarPorDia(fecha: Date) {
+    // Filtrar por día
+    const fechaInicio = new Date(fecha.getFullYear(), fecha.getMonth(), fecha.getDate());
+    const fechaFin = new Date(fecha.getFullYear(), fecha.getMonth(), fecha.getDate() + 1);
+  
+    this.dataPagina = this.data.filter(dato => {
+      const fechaDato = new Date(dato.fecha); // Asegúrate de adaptar esto según la estructura de tu objeto JSON
+      return fechaDato >= fechaInicio && fechaDato < fechaFin;
     });
   }
   
+  
+  filtrarPorSemana(fecha: Date) {
+    // Filtrar por semana
+    const primerDiaSemana = fecha.getDate() - fecha.getDay();
+    const fechaInicio = new Date(fecha.getFullYear(), fecha.getMonth(), primerDiaSemana);
+    const fechaFin = new Date(fecha.getFullYear(), fecha.getMonth(), primerDiaSemana + 7);
+    // Realizar la lógica de filtrado con las fechas obtenidas
+  }
+  
+  filtrarPorMes(fecha: Date) {
+    // Filtrar por mes
+    const fechaInicio = new Date(fecha.getFullYear(), fecha.getMonth(), 1);
+    const fechaFin = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0);
+    // Realizar la lógica de filtrado con las fechas obtenidas
+  }
+  
+  filtrarPorAnio(fecha: Date) {
+    // Filtrar por año
+    const fechaInicio = new Date(fecha.getFullYear(), 0, 1);
+    const fechaFin = new Date(fecha.getFullYear(), 11, 31);
+    // Realizar la lógica de filtrado con las fechas obtenidas
+  }  
 
-  ngOnInit() : void{
-    
+  filtrar(tipoFiltro: string) {
+    const fechaActual = new Date();
+  
+    if (tipoFiltro === 'dia') {
+      this.filtrarPorDia(fechaActual);
+    } else if (tipoFiltro === 'semana') {
+      this.filtrarPorSemana(fechaActual);
+    } else if (tipoFiltro === 'mes') {
+      this.filtrarPorMes(fechaActual);
+    } else if (tipoFiltro === 'anio') {
+      this.filtrarPorAnio(fechaActual);
+    }
+  } 
+  /* **********************************************FIN DEL FILTRO************************************************ */
+  
+  ngOnInit(){
+    this.problemasService.getJsonData().subscribe(data => {//mostrar datos en la tabla
+      this.data = data;//datos
+    });
+    this.dataPagina = [...this.dataCompleta];//logica del arreglo para la paginacion
    }
 
 }
