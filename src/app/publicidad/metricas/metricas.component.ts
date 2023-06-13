12c0a2e10a1d66ad0668b074e8842108eb354bd9
sergiojmnz <61261358+sergiojmnz@users.noreply.github.com>
2023-06-12 22:17:52 -0600

    diego te amo x6

--- src/app/publicidad/metricas/metricas.component.ts
+++ src/app/publicidad/metricas/metricas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+import { Campania } from '../publicidad2/publicidad2.interface';
+import * as campanias from 'src/assets/json/campaniasJson.json'
 
 @Component({
   selector: 'app-metricas',
@@ -6,8 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metricas.component.css']
 })
 export class MetricasComponent implements OnInit {
+  isDropdownOpen: boolean = false;
 
-  constructor() { }
+  campanias: Campania[] = (campanias as any).default;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  getCampaniasbyId(id: string): Campania | undefined {
+    const campania = this.campanias.find(campania => campania.id === id);
+    if (campania && this.campanias.length > 0) {
+      const oferta = this.campanias.find(campania => campania.id === id);
+    }
+  
+    return campania;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  
 
   ngOnInit(): void {
   }
