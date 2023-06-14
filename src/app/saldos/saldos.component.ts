bbcf2ac67f249b2bcc7beee9db61f06d34989600
Alexis2411 <zs19004866@estudiantes.uv.mx>
2023-06-13 21:33:29 -0600

    mas cargos

--- src/app/saldos/saldos.component.ts
+++ src/app/saldos/saldos.component.ts
@@ -1,21 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
-@Component({
-  selector: 'app-saldos',
-  templateUrl: './saldos.component.html',
-  styleUrls: ['./saldos.component.css']
-})
-export class SaldosComponent implements OnInit {
-  monthControl = new FormControl();  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { CargosService } from '../services/cargos.service';
 
 @Component({
